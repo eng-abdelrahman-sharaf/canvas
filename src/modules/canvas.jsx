06d2291bffb438da09274cccbe523a48e9047bcb
Abdelrahman Mostafa Kamel <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2025-09-02 14:12:59 +0300

    Fix some responsiveness issues

--- src/modules/canvas.jsx
+++ src/modules/canvas.jsx
@@ -1,50 +1,64 @@
 import { useEffect, useRef, useState } from "react";
-import redraw from "./scripts/redraw"
+import redraw from "./scripts/redraw";
 
-import "./canvas.css"
+import "./canvas.css";
 
+function Canvas({
+    id,
+    setContext,
+    setCanvas,
+    setEyeDropperCursorCheckbox,
+    drawnList,
+}) {
+    const [canvasHeight, setCanvasHeight] = useState(null);
+    const [canvasWidth, setCanvasWidth] = useState(null);
 
-function Canvas({id, setContext , setCanvas , setEyeDropperCursorCheckbox , drawnList}){
-  const [canvasHeight, setCanvasHeight] = useState(null);
-  const [canvasWidth, setCanvasWidth ] = useState(null);
+    const canvasRef = useRef(null);
+    const eyeDropperCursorCheckboxRef = useRef(null);
 
-  const canvasRef = useRef(null);
-  const eyeDropperCursorCheckboxRef = useRef(null)
-  const canvasContainerRef = useRef(null)
-    
+    const observer = new ResizeObserver(() => {
+        if (!canvasRef.current) return;
 
+        setCanvasWidth(canvasRef.current.offsetWidth);
+        setCanvasHeight(canvasRef.current.offsetHeight);
+    });
 
-    const observer = new ResizeObserver(()=>{
-      if(!canvasContainerRef.current)return
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        context.lineCap = "round";
+        context.lineJoin = "round";
+        redraw(
+            canvasRef.current,
+            canvasRef.current.getContext("2d"),
+            drawnList
+        );
+    }, [canvasHeight, canvasWidth]);
 
-      setCanvasWidth(canvasContainerRef.current.offsetWidth-10)
-      setCanvasHeight(canvasContainerRef.current.offsetHeight-10)
-    })
-
-    useEffect(()=>{
-      const canvas = canvasRef.current
-      const context = canvas.getContext("2d")
-      context.lineCap = "round"
-      context.lineJoin = "round"
-      redraw(canvasRef.current , canvasRef.current.getContext("2d") , drawnList)
-    } , [canvasHeight , canvasWidth])
-
-    useEffect(()=>{
-      observer.observe(canvasContainerRef.current)
-      const eyeDropperCursorCheckbox = eyeDropperCursorCheckboxRef.current
-      setEyeDropperCursorCheckbox(eyeDropperCursorCheckbox)
-      setCanvas(canvasRef.current);
-      setContext(canvasRef.current.getContext("2d"));
-    } , [])
+    useEffect(() => {
+        observer.observe(canvasRef.current);
+        const eyeDropperCursorCheckbox = eyeDropperCursorCheckboxRef.current;
+        setEyeDropperCursorCheckbox(eyeDropperCursorCheckbox);
+        setCanvas(canvasRef.current);
+        setContext(canvasRef.current.getContext("2d"));
+    }, []);
     return (
-      <>
-        <div id="canvas-container" ref={canvasContainerRef}>
-          <header>Canvas</header>
-          <input  type="checkbox" id="eyeDropperCursorCheckbox" ref={eyeDropperCursorCheckboxRef} />
-          <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight} id = {id}></canvas>
-        </div>
-      </>
+        <>
+            <div id="canvas-container">
+                <header>Canvas</header>
+                <input
+                    type="checkbox"
+                    id="eyeDropperCursorCheckbox"
+                    ref={eyeDropperCursorCheckboxRef}
+                />
+                <canvas
+                    ref={canvasRef}
+                    width={canvasWidth}
+                    height={canvasHeight}
+                    id={id}></canvas>
+            </div>
+        </>
     );
-  }
+}
 
-  export default Canvas
+export default Canvas;
--- src/modules/custom-color-picker.jsx
+++ src/modules/custom-color-picker.jsx
@@ -1,32 +1,41 @@
 import { useEffect, useRef } from "react";
 import { SketchPicker } from "react-color";
 
-import "./custom-color-picker.css"
+import "./custom-color-picker.css";
 
-function CustomColorPicker({color , setColor}){
-    
-    const colorPickedRef = useRef(null)
+function CustomColorPicker({ color, setColor }) {
+    const colorPickedRef = useRef(null);
 
-
-    function handleNewColor(color){
+    function handleNewColor(color) {
         setColor(color.hex);
         colorPickedRef.current.style.backgroundColor = color.hex;
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         colorPickedRef.current.style.backgroundColor = color;
-    },[color])
-    return(
+    }, [color]);
+    return (
         <div id="color-picker-container">
-            <input type="checkbox" id="color-picker-checkbox" className="tool-checkbox"/>
-            <label htmlFor="color-picker-checkbox" id="color-picker-checkbox-label" className="tool-checkbox-label">
-                <div className="image" ref={colorPickedRef}></div>
+            <input
+                type="checkbox"
+                id="color-picker-checkbox"
+                className="tool-checkbox"
+            />
+            <label
+                htmlFor="color-picker-checkbox"
+                id="color-picker-checkbox-label"
+                className="tool-checkbox-label">
+                <div className="image picker" ref={colorPickedRef}></div>
             </label>
             <div id="custom-color-picker">
-                <SketchPicker color={color} onChange={handleNewColor} disableAlpha = {true} />
+                <SketchPicker
+                    color={color}
+                    onChange={handleNewColor}
+                    disableAlpha={true}
+                />
             </div>
         </div>
-    )
+    );
 }
 
-export default CustomColorPicker
+export default CustomColorPicker;
